ab0a4721a881d6569fba6e484b0b4d3b97e3cba3
effe <effe@shiva.(none)>
2010-09-14 15:09:31 +0200

    distutils setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Escapyo',
+      version='0.1',
+      author='Yellow at wubook dot net',
+      package_dir={'': 'escapyo'},
+      url= 'http://en.wubook.net/',
+      packages=['escapyo'],
+      )
+
